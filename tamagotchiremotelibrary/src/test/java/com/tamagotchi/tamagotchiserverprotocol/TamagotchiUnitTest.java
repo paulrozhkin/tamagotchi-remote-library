bc571bb7968bd9c8a7eb9a3c9ead7fea0d4e190a
Paul Rozhkin <blackiiifox@gmail.com>
2020-05-23 18:38:49 +0300

    Add permissions

--- tamagotchiremotelibrary/src/test/java/com/tamagotchi/tamagotchiserverprotocol/TamagotchiUnitTest.java
+++ tamagotchiremotelibrary/src/test/java/com/tamagotchi/tamagotchiserverprotocol/TamagotchiUnitTest.java
@@ -1,6 +1,14 @@
 package com.tamagotchi.tamagotchiserverprotocol;
 
+import com.tamagotchi.tamagotchiserverprotocol.models.RestaurantModel;
+
 import org.junit.Test;
+
+import java.net.SocketException;
+import java.util.List;
+
+import retrofit2.HttpException;
+import retrofit2.http.HTTP;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +21,21 @@ public class TamagotchiUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test_connection_success() {
+        boolean isConnectionFailed = false;
+
+        try {
+            List<RestaurantModel> restaurants = RestaurantClient.getInstance().getRestaurantsService().getAllRestaurants().blockingGet();
+        }
+        catch (HttpException ex) {
+            if (ex.code() != 401) {
+                isConnectionFailed = true;
+            }
+        }
+
+        assertFalse(isConnectionFailed);
     }
 }
